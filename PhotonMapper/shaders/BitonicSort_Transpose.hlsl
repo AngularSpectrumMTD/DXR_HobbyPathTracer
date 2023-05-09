82deb696590680fd21802fcb3d9878a8309e93e5
DESKTOP-9UJLOGG\theve <integral11anddifferental20@yahoo.co.jp>
2023-05-09 23:26:27 +0900

    rename LDS buffer of BitonicSort

--- PhotonMapper/shaders/BitonicSort_Transpose.hlsl
+++ PhotonMapper/shaders/BitonicSort_Transpose.hlsl
@@ -3,13 +3,13 @@
 RWStructuredBuffer<uint2> gInput : register(u0);
 RWStructuredBuffer<uint2> gOutput : register(u1);
 
-groupshared uint2 transpose_shared_data[TRANSPOSE_BLOCK_SIZE * TRANSPOSE_BLOCK_SIZE];
+groupshared uint2 LDStransposeTbl[TRANSPOSE_BLOCK_SIZE * TRANSPOSE_BLOCK_SIZE];
 
 [numthreads(TRANSPOSE_BLOCK_SIZE, TRANSPOSE_BLOCK_SIZE, 1)]
 void matrixTranspose(uint3 dtid : SV_DispatchThreadID, uint3 gtid : SV_GroupThreadID, uint gid : SV_GroupIndex)
 {
-    transpose_shared_data[gid] = gInput[dtid.y * gBitonicParam.width + dtid.x];
+    LDStransposeTbl[gid] = gInput[dtid.y * gBitonicParam.width + dtid.x];
     GroupMemoryBarrierWithGroupSync();
     uint2 XY = dtid.yx - gtid.yx + gtid.xy;
-    gOutput[XY.y * gBitonicParam.height + XY.x] = transpose_shared_data[gtid.x * TRANSPOSE_BLOCK_SIZE + gtid.y];
+    gOutput[XY.y * gBitonicParam.height + XY.x] = LDStransposeTbl[gtid.x * TRANSPOSE_BLOCK_SIZE + gtid.y];
 }
