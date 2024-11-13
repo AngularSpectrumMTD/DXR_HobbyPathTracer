0cee7cc64af2b1bb966f93da1ea07ded5f0d8e3a
DESKTOP-9UJLOGG\theve <integral11anddifferental20@yahoo.co.jp>
2024-11-13 23:40:23 +0900

    updated default type of scene

--- PhotonMapper/DxrPhotonMapper.cpp
+++ PhotonMapper/DxrPhotonMapper.cpp
@@ -59,7 +59,7 @@ void DxrPhotonMapper::UpdateWindowText()
 
 void DxrPhotonMapper::Setup()
 {
-    mSceneType = SceneType_BistroExterior;
+    mSceneType = SceneType_Kitchen;
 
     mIsUseIBL = true;
     mRecursionDepth = min(5, REAL_MAX_RECURSION_DEPTH);
