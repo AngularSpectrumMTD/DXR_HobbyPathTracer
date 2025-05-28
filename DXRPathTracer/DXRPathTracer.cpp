e90c00f47a38ff1dffbb9bf04f07e90b991fa0ec
DESKTOP-9UJLOGG\theve <integral11anddifferental20@yahoo.co.jp>
2025-05-28 23:55:13 +0900

    changed the default 3D scene

--- DXRPathTracer/DXRPathTracer.cpp
+++ DXRPathTracer/DXRPathTracer.cpp
@@ -59,7 +59,7 @@ void DXRPathTracer::UpdateWindowText()
 
 void DXRPathTracer::Setup()
 {
-    mSceneType = SceneType_BistroExterior;
+    mSceneType = SceneType_PTTestRoom4;
 
     mIsUseIBL = true;
     mRecursionDepth = min(6, REAL_MAX_RECURSION_DEPTH);
